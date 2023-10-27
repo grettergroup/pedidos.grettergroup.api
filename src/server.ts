c9b6ec960e4d86b200a2c2159052bf4b202a69a3
Flavio Luis Greter <flavio.greter@agendaassessoria.com.br>
2023-10-27 17:35:01 -0400

    Prisma config

--- src/server.ts
+++ src/server.ts
@@ -31,4 +31,4 @@ app.use((err: Error, req: Request,  res: Response, next: NextFunction) =>{
     })
 })
 
-app.listen(3333, () => console.log('Servidor Online!'));
+app.listen(3333, () => console.log('Servidor Online! Porta : 3333'));
